605e74a0c77f7ad8dc7c4db1f3f9c791d283b222
Gouthum Karadi <goukaradi@hotmail.com>
2020-01-17 11:39:08 -0500

    Reorganized directory structure and added files.

--- data_model.py
+++ data_model.py
@@ -42,32 +42,38 @@ def db_initialize():
 
 # Drop table
 def db_drop_table(table_name):
+    db_connect()
     db.drop_tables([table_name])
     return
 
 # Create table
 def db_create_table(table_name):
+    db_connect()
     db.create_tables([table_name])
     return 
 
 # Create Recordsets
 def create_scrape(url, heading_tag, text_tag, link_tag):
+    db_connect()
     scrape_record = Scrape(url=url, heading_tag=heading_tag, text_tag=text_tag, link_tag=link_tag)
     scrape_record.save()
     return scrape_record
 
 def create_result(url, headings, text, links):
+    db_connect()
     result_record = Results(url=url, headings=headings, text=text, links=links)
     result_record.save()
     return result_record
 
 # Read: (.get() and .select()) get = 1 record, select = multiple
 def get_scrape(url):
+    db_connect()
     result = Scrape.get(Scrape.url == url)
     print(result)
     return result
 
 def select_results(url):
+    db_connect()
     results = Results.select(Results.url == url)
     for result in results:
         print(result)
@@ -75,15 +81,26 @@ def select_results(url):
 
 # Update
 def update_scrape(url):
+    db_connect()
     update_query = Scrape.update().where(Scrape.url == url)
     update_query.execute()
     return 
 
 # Delete
 def delete_scrape(url):
+    db_connect()
     delete_query = Scrape.delete().where(Scrape.url == url)
 
 # Print results
-print(f"Karadi was born on {karadi.birthday}")
-print(f"Maddy is a {maddy.animal_type}")
+def print_scrape(url):
+    db_connect()
+    scrape = get_scrape(url)
+    print(scrape)
+    return
+
+def print_records(url):
+    db_connect()
+    results = select_results(url)
+    print(results)
+    return
 
--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 import click
 import scraper
+# import data_model
 
 @click.command()
 @click.option('--h_tag', default="h3", help='Heading TAG Number, default h3')
 @click.option('--link_tag', default="li a", help='Link TAG, default li a')
 @click.option('--text_tag', default="li", help='Text TAG, default li')
-@click.option('--target', default="https://owasp.org/www-project-top-ten/",
+@click.option('--target_url', default="https://owasp.org/www-project-top-ten/",
               help='URL to scrape, default https://owasp.org/www-project-top-ten/')
 
-def scrape(h_tag, link_tag, text_tag, target):
+def scrape(h_tag, link_tag, text_tag, target_url):
     """A Program to Scrape a website"""
-    target = scraper.create_target(target)
+    # url = target_url
+    target = scraper.create_target(target_url)
     page = scraper.get_page(target)
     soup = scraper.get_soup(page)
     headings = scraper.get_heading(soup, h_tag)
     text = scraper.get_texts(soup, text_tag)
     links = scraper.get_links(soup, link_tag)
+    # data_model.create_scrape(target, h_tag, text_tag, link_tag)
+    # data_model.create_result(target, headings, text, links)
+    # data_model.print_scrape(url)
+    # data_model.print_records(url)
+
+    # Print Tests DELETE
     scraper.print_headings(headings)
     scraper.print_text(text)
     scraper.print_links(links)
