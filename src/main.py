dedd65bdd2b8365b29d755bd6712f88c8c5eb87b
Gouthum Karadi <goukaradi@hotmail.com>
2020-01-17 10:03:42 -0500

    Successfully run with options from the shell.  SUCCESS.  Now adding DB model in postgres.

--- src/main.py
+++ src/main.py
@@ -8,16 +8,16 @@ import scraper
 @click.option('--target', default="https://owasp.org/www-project-top-ten/",
               help='URL to scrape, default https://owasp.org/www-project-top-ten/')
 
-def scrape(target, h_tag, text_tag, target):
+def scrape(h_tag, link_tag, text_tag, target):
     """A Program to Scrape a website"""
     target = scraper.create_target(target)
     page = scraper.get_page(target)
     soup = scraper.get_soup(page)
     headings = scraper.get_heading(soup, h_tag)
-    texts = scraper.get_texts(soup, text)
+    text = scraper.get_texts(soup, text_tag)
     links = scraper.get_links(soup, link_tag)
     scraper.print_headings(headings)
-    scraper.print_text(texts)
+    scraper.print_text(text)
     scraper.print_links(links)
 
 if __name__ == '__main__':
